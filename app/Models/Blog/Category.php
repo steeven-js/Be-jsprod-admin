bb9b8d4b10ab29400c57c64e80493db900bca74e
steeven-js <jacques.steeven@gmail.com>
2024-04-14 14:03:59 -0400

    Mettre à jour les ressources des catégories dans CategoryFactory.php

--- app/Models/Blog/Category.php
+++ app/Models/Blog/Category.php
@@ -2,9 +2,10 @@
 
 namespace App\Models\Blog;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Blog\Study;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
@@ -26,5 +27,11 @@ class Category extends Model
     public function posts(): HasMany
     {
         return $this->hasMany(Post::class, 'blog_category_id');
+    }
+
+    /** @return HasMany<Study> */
+    public function studies(): HasMany
+    {
+        return $this->hasMany(Study::class, 'blog_category_id');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\TagController;
 use App\Http\Controllers\API\PostController;
 use App\Http\Controllers\Api\StudyController;
+use App\Http\Controllers\Api\StudyCategoryController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,3 +14,4 @@ Route::get('/user', function (Request $request) {
 Route::apiResource('posts', PostController::class);
 Route::apiResource('tags', TagController::class);
 Route::apiResource('studies', StudyController::class);
+Route::apiResource('categories', StudyCategoryController::class);
