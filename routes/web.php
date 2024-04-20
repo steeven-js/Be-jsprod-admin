3d460f7ed6c21594dc8998b2c14962c06687b784
steeven-js <jacques.steeven@gmail.com>
2024-04-20 12:52:42 -0400

    Mise à jour de la route d'accueil pour rediriger vers /admin

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin');
 });
 
 Route::get('/dashboard', function () {
